f220301b8dde653ffaeabf32c1132d42b0a9b326
Chandler Kilpatrick <crskilpatrick807@gmail.com>
2024-05-08 13:27:12 -0500

    f8

--- netlify/functions/random_pokemon.js
+++ netlify/functions/random_pokemon.js
@@ -1,28 +1,60 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-exports.handler = async (event, context) => {
+// exports.handler = async (event, context) => {
+//   const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
+
+//   try {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+//     const pokemonList = data.results;
+//     const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
+//     const pokemonName = randomPokemon.name;
+
+//     const imageUrl = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`;
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         name: pokemonName,
+//         imageUrl: imageUrl
+//       })
+//     };
+
+//   } catch (error) {
+//     // output to netlify function log
+//     console.error("Error fetching pokemon image:", error);
+//     return { statusCode: 422, body: String(error) };
+//   }
+// };
+// const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+exports.handler = async function(event, context) {
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
 
   try {
     const response = await fetch(apiUrl);
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ message: "Error fetching pokemon list" })
+      };
+    }
     const data = await response.json();
     const pokemonList = data.results;
     const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
     const pokemonName = randomPokemon.name;
-
     const imageUrl = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`;
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        name: pokemonName,
-        imageUrl: imageUrl
-      })
+      body: JSON.stringify({ name: pokemonName, imageUrl: imageUrl })
     };
-
   } catch (error) {
-    // output to netlify function log
     console.error("Error fetching pokemon image:", error);
-    return { statusCode: 422, body: String(error) };
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error fetching pokemon image" })
+    };
   }
 };
