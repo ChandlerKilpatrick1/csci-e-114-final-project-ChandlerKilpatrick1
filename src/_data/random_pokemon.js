a4c3b029a25d92119877299d1d0f47bd48f9a362
Chandler Kilpatrick <crskilpatrick807@gmail.com>
2024-05-08 12:09:00 -0500

    fix?

--- src/_data/random_pokemon.js
+++ src/_data/random_pokemon.js
@@ -1,26 +1,26 @@
-const eleventyFetch = require("@11ty/eleventy-fetch");
+// const eleventyFetch = require("@11ty/eleventy-fetch");
 
-module.exports = async function() {
-  const baseUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
-  try {
-    const response = await eleventyFetch(baseUrl, {
-      duration: '1d', 
-      type: 'json'
-    });
+// module.exports = async function() {
+//   const baseUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
+//   try {
+//     const response = await eleventyFetch(baseUrl, {
+//       duration: '1d', 
+//       type: 'json'
+//     });
 
-    const pokemonList = response.results;
-    const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
-    const pokemonName = randomPokemon.name;
+//     const pokemonList = response.results;
+//     const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
+//     const pokemonName = randomPokemon.name;
 
-    // Construct the image URL
-    const imageUrl = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`;
+//     // Construct the image URL
+//     const imageUrl = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`;
 
-    return {
-      name: pokemonName,
-      imageUrl: imageUrl
-    };
-  } catch (error) {
-    console.error('Error fetching the list of Pokémon:', error);
-    return {};
-  }
-};
+//     return {
+//       name: pokemonName,
+//       imageUrl: imageUrl
+//     };
+//   } catch (error) {
+//     console.error('Error fetching the list of Pokémon:', error);
+//     return {};
+//   }
+// };
