bd9089fdb015156b0c74b3a0a0612f90931c690f
Chandler Kilpatrick <crskilpatrick807@gmail.com>
2024-05-08 14:05:06 -0500

    f9

--- netlify/functions/random_pokemon.js
+++ netlify/functions/random_pokemon.js
@@ -1,34 +1,7 @@
-// const fetch = require('node-fetch');
 
-// exports.handler = async (event, context) => {
-//   const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
+// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-//   try {
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     const pokemonList = data.results;
-//     const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
-//     const pokemonName = randomPokemon.name;
-
-//     const imageUrl = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`;
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({
-//         name: pokemonName,
-//         imageUrl: imageUrl
-//       })
-//     };
-
-//   } catch (error) {
-//     // output to netlify function log
-//     console.error("Error fetching pokemon image:", error);
-//     return { statusCode: 422, body: String(error) };
-//   }
-// };
-// const fetch = require('node-fetch');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
+import fetch from 'node-fetch';
 exports.handler = async function(event, context) {
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
 
